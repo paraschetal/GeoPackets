431fa0d71de7244022038891479364207d01b7b5
Ketan Gupta <ketan19972010@gmail.com>
2015-12-12 13:45:38 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ os.system("sudo pip install dpkt")
 
 os.system("sudo pip install pygeoip")
 
-os.system("gunzip Geo*.dat.gz")
+os.system("gunzip -k GeoIP.dat.gz")
 os.system("sudo mkdir /opt/GeoIP")
-os.system("sudo mv Geo* /opt/GeoIP")
-os.system("cd /opt/GeoIp")
-os.system("sudo mv GeoIP.dat Geo.dat")
+os.system("sudo mv GeoIP.dat /opt/GeoIP")
+os.system("sudo mv /opt/GeoIP/GeoIP.dat /opt/GeoIP/Geo.dat")
 
 #Fin.
