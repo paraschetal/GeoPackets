675d3caa46a1ca6d9c24384b015e08bde5f78c05
paraschetal <paras.chetal@gmail.com>
2016-01-05 23:02:05 +0530

    Adds LineString to show the WEB

--- GeoPackets.py
+++ GeoPackets.py
@@ -1,4 +1,5 @@
 import dpkt, socket, pygeoip, optparse
+
 gi = pygeoip.GeoIP('/opt/GeoIP/Geo.dat')
 def retKML(ip):
 	rec=gi.record_by_name(ip)
@@ -12,31 +13,98 @@ def retKML(ip):
 			'<coordinates>%6f,%6f</coordinates>\n'
 			'</Point>\n'	
 			'</Placemark>\n'
-			)%(ip+"("+rec['city']+", "+rec['country_name']+")",longitude,latitude)
+			)%(ip,longitude,latitude)
+		# print ip
 		return kml
-	except:
+	except :
 		return ''
+def drawLines(srcORdstList):
+	linePoints=[]
+	count=0
+	for i in srcORdstList:
+		for j in srcORdstList:
+			try:
+				if i==j:
+					continue
+				rec1=gi.record_by_name(i)
+				rec2=gi.record_by_name(j)
+				
+				lat1=rec1['latitude']
+				lon1=rec1['longitude']
+				lat2=rec2['latitude']
+				lon2=rec2['longitude']
+				
+				linePoint=str(lon1)+','+str(lat1)+','+str(0)+" "+str(lon2)+","+str(lat2)+",0."+'\n'
+				# print linePoint+' '+i+' to '+j
+				linePoints.append(linePoint)
+				# count+=1
+			except Exception,e:
+				pass
+	kml=''
+	# print len(linePoints)		
+	# print count			
+	# kml=('')
+	for i in linePoints:
+		# print i
+		kml+=(
+			'<Placemark>\n'
+			'<name></name>\n'
+			'<LineString>\n'
+			'<tessellate>1</tessellate>\n'
+			'<coordinates>\n'
+			)	
+		
+		kml+=i
+			
+		kml+=(	'</coordinates>\n'
+				'</LineString>\n'
+			 )	
+		kml=kml+('<Style>\n'
+			'<LineStyle>\n'
+			'<width>1</width>\n'
+			'<color>#ff0000ff</color>\n'
+			'</LineStyle>\n'
+			'</Style>\n'
+			'</Placemark>\n'
+		)				
+	return(kml)			
 def plotIPs(pcap):
 	kmlPts=''
 	srcList=[]
 	dstList=[]
+	srcORdstList=[]
 	for (ts,buf) in pcap:
 		try:
 			eth=dpkt.ethernet.Ethernet(buf)
 			ip=eth.data
+			
 			src=socket.inet_ntoa(ip.src)
-			if src not in srcList:
-				srcList.append(src)
+			rec=gi.record_by_name(src)
+			if src not in srcORdstList and rec['latitude']!=None:
+				srcORdstList.append(src)
+			# if src not in srcList:
+			# 	srcList.append(src)
 			dst=socket.inet_ntoa(ip.dst)
-			if dst not in dstList:
-				dstList.append(dst)
+			rec=gi.record_by_name(dst)
+			if dst not in srcORdstList and rec['latitude']!=None:
+				srcORdstList.append(dst)
+			# if dst not in dstList:
+			# 	dstList.append(dst)
+
 		except:
 			pass
-	for srcs in srcList:
-		kmlPts=kmlPts+retKML(srcs)		
-	for dsts in dstList:
-		kmlPts=kmlPts+retKML(dsts)	
-	return kmlPts
+	for node in srcORdstList:
+		# print node
+		kmlPts=kmlPts+retKML(node)
+	# my_ip = urllib2.urlopen('http://ip.42.pl/raw').read()
+	kmlMap=kmlPts+drawLines(srcORdstList)
+	for i in srcORdstList:
+		rec=gi.record_by_name(i)
+		# try:
+		# 	print rec['city']+rec['country_name']
+		# except:
+		# 	print i	
+	return kmlMap
 def main():
 	parser=optparse.OptionParser('usage%prog -p ,pcap file.')
 	parser.add_option('-p', dest='pcapFile' ,type='string', help='specify pcap filename')
